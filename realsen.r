667526fa8acd6e8f4ba83cd953d81b29f675924b
bdavisg <93791564+bdavisg@users.noreply.github.com>
2021-11-29 15:51:50 -0500

    Update realsen.r

--- realsen.r
+++ realsen.r
@@ -20,6 +20,7 @@ parallel:::setDefaultClusterOptions(setup_strategy = "sequential")
 #load processed data and previous Stan file
 source('sim.data.r')
 fit_predict <- readRDS("fit_predict2.rds") #this will load the fit model that I already ran
+fit_sens <- readRDS("fit_sens1.rds") #this will load the fit model that I already ran
 
 #Extract samples and calculate the mean for GP model parameters to use in fixed GP model
 samples<-extract(fit_predict)
@@ -29,36 +30,42 @@ sigma<-mean(samples$sigma)
 
 #Create vector x1 and x2 from sim.data.r
 #Must use at least 226 data points or Senitivity package will not work
-X1<-as.data.frame(stan_data$x1[1:226,])
-X2<-as.data.frame(stan_data$x1[226:1,])
+X1<-as.data.frame(stan_data$x1[1:295,3:15]) #Does not include experimental variables v and theta
+X2<-as.data.frame(stan_data$x1[295:1,3:15]) #Does not include experimental variables v and theta
 
 #Create vector X of samples to simulation from fixed GP model. Called x2 in stan model.
 sens<-sobol2007(model=NULL,X1,X2,nboot=1000)
-x2<-sens$X #Becomes vector x2 in Stan model
+x2<-sens$X #Becomes vector x2 in Stan model of all parameters except velocity and angle
+
+v<-c(stan_data$x1[1:295,1],stan_data$x1[295:1,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1],stan_data$x1[1:295,1])
+t<-c(stan_data$x1[1:295,2],stan_data$x1[295:1,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2],stan_data$x1[1:295,2])
+x2sim<-cbind(v,t,x2) #combining sensitivity analysis vector with velocity and angle data needed to run GP model
 
 #Create new vector of data for Stan model to run
-N2<-nrow(x2)
-D2<-ncol(x2)
+N2<-nrow(x2sim)
+D2<-ncol(x2sim)
 
 #Load data into a single list for Stan
-stan_data1 <- list(N1=stan_data$N1,D1=stan_data$D1,N2=N2,D2=D2,x1=stan_data$x1,y1=stan_data$y1,x2=x2,rho=rho,alpha=alpha,sigma=sigma)
+stan_data1 <- list(N1=stan_data$N1,D1=stan_data$D1,N2=N2,D2=D2,x1=stan_data$x1,y1=stan_data$y1,x2=x2sim,rho=rho,alpha=alpha,sigma=sigma)
 #stan_data1 <- list(N1=stan_data$N1,D1=stan_data$D1,N2=stan_data$N2,D2=stan_data$D2,x1=stan_data$x1,y1=stan_data$y1,x2=stan_data$x2,rho=rho,alpha=alpha,sigma=sigma)
 
 #Run stan model to generate samples
-fit_sens <- stan(file="perf-sens.stan",data=stan_data1,iter=2000, seed=249, refresh=10, control = list(max_treedepth=11))
+fit_sens <- stan(file="perf-sens2.stan",data=stan_data1, chains=1, iter=3000, warmup=200, seed=2491, refresh=10, control = list(max_treedepth=11))
 
 #Check model performance
 util$check_all_diagnostics(fit_sens)
 
+print(fit_sens)
+
 #Save Stan model results for use in future sessions
-saveRDS(fit_sens,"fit_sens1.rds")
+saveRDS(fit_sens,"fit_sens3000.rds")
 
 #Extract results for y2
 y2 <- extract(fit_sens)$y2[1,]
 
 #Plot results to make sure that data looks reasonable
-plot(stan_data$x1,stan_data$y1)
-points(stan_data$x2,y2,col='black',pch=16, cex=1)
+plot(stan_data$x1[1:295,1],stan_data$y1[1:295])
+points(x2sim[1:295,1],y2[1:295],col='black',pch=16, cex=1)
 
 #Complete Sensitivity Analysis
 tell(sens,y2)
